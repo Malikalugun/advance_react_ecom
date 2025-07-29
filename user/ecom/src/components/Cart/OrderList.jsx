8f3a3a73d13a5da38ddbb3862372ebf9e806316f
Malikalugun <annamalikalugun@gmail.com>
2025-07-29 07:16:07 +0530

    notification model
    notification model  updated

--- user/ecom/src/components/Cart/OrderList.jsx
+++ user/ecom/src/components/Cart/OrderList.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import AppURL from "../../api/AppURL";
 import axios from "axios";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Modal } from "react-bootstrap";
 
 export class OrderList extends Component {
   constructor() {
     super();
     this.state = {
       ProductData: [],
+      show: false,
+      NotificationData: [],
+      isLoading: "",
+      mainDiv: "d-none",
+      Notificationmsg: "",
+      Notificationtitle: "",
+      Notificationdate: "",
     };
   }
 
@@ -23,6 +30,21 @@ export class OrderList extends Component {
       });
   }
 
+  handleClose = () => {
+    this.setState({ show: false });
+  };
+
+  handleShow = (event) => {
+    this.setState({ show: true });
+    let Nmsg = event.target.getAttribute("data-message");
+    let Ntitle = event.target.getAttribute("data-title");
+    let Ndate = event.target.getAttribute("data-date");
+    this.setState({
+      Notificationmsg: Nmsg,
+      Notificationtitle: Ntitle,
+      Notificationdate: Ndate,
+    });
+  };
   render() {
     const MyList = this.state.ProductData;
 
@@ -40,20 +62,39 @@ export class OrderList extends Component {
               {productList.total_price}$
             </h6>
             <h6>Status = {productList.order_status}</h6>
-            <Button className="btn btn-danger">Post Review</Button>
+            <Button className="btn btn-danger" onClick={this.handleShow}>
+              Post Review
+            </Button>
           </Card.Body>
         </Card>
       </Col>
     ));
 
     return (
-      <Container>
-        <div className="section-title text-center mb-4">
-          <h2>Product Order History</h2>
-        </div>
-
-        <Row>{MyView}</Row>
-      </Container>
+      <Fragment>
+        <Container>
+          <div className="section-title text-center mb-4">
+            <h2>Product Order History</h2>
+          </div>
+          <Row>{MyView}</Row>
+        </Container>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <h6>
+              <i className="fa fa-bell"></i> Post Your Review
+            </h6>
+          </Modal.Header>
+          <Modal.Body>
+            <h6>Review</h6>
+            <p>review</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Fragment>
     );
   }
 }
